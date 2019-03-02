0817b8e060ee83255b942230cff467c4ec816820
ZacEmerzian <45469117+ZacEmerzian@users.noreply.github.com>
2019-03-02 14:37:17 -0800

    Inventory 1.0
    About to add click to buy, no more drag and drop.

--- Assets/Inventory/ItemManager.cs
+++ Assets/Inventory/ItemManager.cs
@@ -17,36 +17,39 @@ public class ItemManager : MonoBehaviour
         {
             Instantiate(pair.prefab, new Vector3((float)(-5.75 + (2 * (i % 3))), (float)(3 - 2*(i / 3)), 0), Quaternion.identity);
             i++;
+
+
         }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // divides all items into store or closet lists
-        if (true /* TODO: INSERT CODE THAT CHECKS IF THE STORE/CLOSET IS OPEN */)
+        if (true /* TODO: INSERT CODE THAT CHECKS IF THE STORE/CLOSET IS OPEN*/ )
         {
             storeItems.Clear();
             closetItems.Clear();
-            foreach (ItemLog pair in AllItemList)
+            foreach ( ItemBehavoir item in GameObject.FindObjectsOfType<ItemBehavoir>() )
             {
-                if (pair.prefab.GetComponent<ItemBehavoir>().location == ItemBehavoir.foundIn.store)
+                if (item.location == ItemBehavoir.foundIn.store)
                 {
-                    storeItems.Add(pair.prefab);
+                    storeItems.Add(item.gameObject);
                 }
                 else /*if (pair.prefab.GetComponent<ItemBehavoir>().location == ItemBehavoir.foundIn.closet)*/
                 {
-                    closetItems.Add(pair.prefab);
+                    closetItems.Add(item.gameObject);
                 }
             }
         }
-
 
         // organize the items in the store
         int i = 0;
         foreach (GameObject item in storeItems)
         {
-            item.transform.Translate(new Vector3( (float) (-5.75 + (2 * (i % 3))), (float) (3 - 2*(i/3)), 0) * Time.deltaTime);
+            item.transform.position = Vector3.MoveTowards(transform.position, new Vector3((float)(-5.75 + (2 * (i % 3))), (float)(3 - 2 * (i / 3)), 0), 100);
             i++;
         }
 
@@ -54,7 +57,7 @@ public class ItemManager : MonoBehaviour
         int j = 0;
         foreach (GameObject item in closetItems)
         {
-            item.transform.Translate(new Vector3((float)(1.75 + (2 * (j % 3))), (float)(3 - 2*(j / 3)), 0) * Time.deltaTime);
+            item.transform.position = Vector3.MoveTowards(transform.position, new Vector3((float)(1.75 + (2 * (j % 3))), (float)(3 - 2 * (j / 3)), 0), 100);
             j++;
         }
 
