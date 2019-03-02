c865679c106bc0529672cff0861aa91717b97e86
YitongHuGpm20 <yhu216@ucsc.edu>
2019-03-01 21:52:29 -0800

    Go home
    Created spawn manager, try to move value of timebar but failed

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
     public int day = 0;
     public int fishCoin = 10;
+    public int countDown = 5;
+    public Slider timeBar;
 
     private void Awake()
     {
@@ -19,6 +22,9 @@ public class GameManager : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-    
 
+    private void Update()
+    {
+        timeBar.value = (day - 1) * (1 / (countDown - 1));
+    }
 }
