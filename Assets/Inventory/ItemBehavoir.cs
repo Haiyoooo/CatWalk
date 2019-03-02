a920e0177ada01c8692cd91b5274d67444567535
ZacEmerzian <45469117+ZacEmerzian@users.noreply.github.com>
2019-03-01 22:42:30 -0800

    Inventory Scene 0.1
    Can move the base item prefabs around with the mouse.  An item manager script exists, not yet linked to the item prefabs.

---
+++ Assets/Inventory/ItemBehavoir.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehavoir : MonoBehaviour
+{
+
+    bool mouseHeld;
+    enum foundIn { store, closet };
+    foundIn location;
+    enum style { Western, Goth, Pirate/*, Formal, Neon, Skater, Sporty, Cute, Graceful, Southern, Royal */}
+    bool equipped;
+    enum putOn { head, body };
+    putOn wornOn;
+    bool mouseOver;
+
+    void Start()
+    {
+        mouseHeld = false;
+        location = foundIn.store;
+        equipped = false;
+        mouseOver = false;
+    }
+
+    
+    void Update()
+    {
+        // Mouse grab code
+
+        // grabs the item
+        if ( mouseOver && Input.GetMouseButtonDown(0) )
+        {
+            mouseHeld = true;
+        }
+
+        // move with the mouse
+        if (mouseHeld)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector3.Scale(transform.position, new Vector3(1, 1, 0));
+
+            // releases the item
+            if (Input.GetMouseButtonUp(0))
+            {
+                mouseHeld = false;
+            }
+        }
+
+    }
+
+    protected void OnMouseOver()
+    {
+        mouseOver = true;
+    }
+    protected void OnMouseExit()
+    {
+        mouseOver = false;
+    }
+
+}
---
+++ Assets/Inventory/ItemManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    public ItemLog[] AllItemList = new ItemLog[3];
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+[System.Serializable]
+public class ItemLog
+{
+    public GameObject prefab;
+    public int cost;
+}
