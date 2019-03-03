b1278dc58c233af0abc747f76549bfec34e96395
YitongHuGpm20 <yhu216@ucsc.edu>
2019-03-03 12:24:13 -0800

    Debt system done

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -8,24 +8,21 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
     public float day = 0;
-    public int fishCoin = 10;
-    private float countDown = 7;
+    public float countDown = 7;
     public Slider timeBar;
     private float timeValue;
     private float timeLength;
     private float dayIndex = 1;
 
-    public int debt;
+    public int fishCoin = 10;
+    public int debt = 20;
+    private bool isPaied = false;
 
-    private Text cashMoney;
-    private Text debtText;
+    public GameObject cashText;
+    public GameObject debtText;
     private Text fameStatusText;
-
-    public AudioSource[] sounds;
-
-    public AudioSource equip;
-    public AudioSource unequip;
-    public AudioSource item_select;
+    public GameObject endGame;
+    public GameObject endGameText;
 
     private void Awake()
     {
@@ -38,36 +35,53 @@ public class GameManager : MonoBehaviour
             Destroy(this.gameObject);
         }
 
-        cashMoney = GameObject.Find("Cash").GetComponent<Text>();
-        debtText = GameObject.Find("Debt").GetComponent<Text>();
         fameStatusText = GameObject.Find("Fame Status").GetComponent<Text>();
-
-        sounds = GetComponents<AudioSource>();
-
-        //AUDIO
-        equip = sounds[1];
-        unequip = sounds[2];
-        item_select = sounds[3];
 
     }
 
-
     private void Update()
+    {
+        TimebarValue();
+        DisplayCashDebt();
+        PayOnDeadline();
+    }
+
+    private void TimebarValue()
     {
         dayIndex = day % countDown - 1;
         if (day % countDown == 0)
             timeBar.value = 1;
         else
             timeBar.value = dayIndex * (1 / (countDown - 1));
-        //if (timeBar.value > 1) timeBar.value = 0;
-
-        displayText();
     }
 
-    private void displayText()
+    private void DisplayCashDebt()
     {
-        cashMoney.text = "You have: " + fishCoin;
-        debtText.text = "You owe: " + debt;
-        fameStatusText.text = "Nobody knows you...";
+        debtText.GetComponent<Text>().text = "You own: " + debt + " FishCoin";
+        cashText.GetComponent<Text>().text = "You have: " + fishCoin + " FishCoin";
+    }
+
+    private void PayOnDeadline()
+    {
+        if (day == countDown && !isPaied)
+        {
+            fishCoin -= debt;
+            isPaied = true;
+            if (fishCoin <= 0)
+            {
+                endGame.SetActive(true);
+                endGameText.GetComponent<TextMeshProUGUI>().text = "You lost lah!";
+            }
+            else
+            {
+                endGame.SetActive(true);
+                endGameText.GetComponent<TextMeshProUGUI>().text = "Meow, you won!";
+            }
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
