bd67ea65f9e0b6b304be76df1400c2e87c6e6962
Haiyoooo <38852079+Haiyoooo@users.noreply.github.com>
2019-03-03 16:57:53 -0800

    Popup Messages Prefabs
    - Split the image up into parts so text can be changed dynamically
    - Added an "OK" button
    - Combined job popup & party popup scripts & prefabs into 'Pop Up'

---
+++ Assets/PopUp Messages/PartyPopUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyPopUp : MonoBehaviour
+{
+    private Party party;
+    private Job job;
+
+    void Start()
+    {
+        party = transform.parent.parent.GetComponent<Party>();
+        job = transform.parent.parent.GetComponent<Job>();
+
+    }
+
+    public void hidePopUp()
+    {
+        //remove the event if success.... TODO: refactor. needed the if function because my script is stupid
+        if (transform.parent.parent.tag == "Job")
+        {
+            job.disappearOnSuccess();
+        }
+        else
+        {
+            party.disappearOnSuccess();
+        }
+
+
+        Destroy(transform.parent.gameObject); //hide the pop up message
+    }
+}
