dc0237e9b6147b52106d7a633103b7aecea887dc
Haiyoooo <38852079+Haiyoooo@users.noreply.github.com>
2019-03-04 08:46:41 -0800

    Timebar

---
+++ Assets/Timebar/Timebar_Animation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timebar_Animation : MonoBehaviour
+{
+    [Header("Icon Images")]
+    [SerializeField] private Sprite future; //day hasn't been passed yet
+    [SerializeField] private Sprite past; //day is in the past
+    private Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        image.sprite = future;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Time Bar Handle") //the arrow indicator, object name 'handle'
+        {
+            image.sprite = past;
+        }
+    }
+
+    public void ResetDayMarkerColor()
+    {
+        image.sprite = future;
+    }
+}
