9445cb6599412d959da156a6c62733308345dbe6
Haiyoooo <38852079+Haiyoooo@users.noreply.github.com>
2019-03-07 14:17:24 -0800

    Added Comments to "ArrowScroll"

--- Assets/ArrowScroll.cs
+++ Assets/ArrowScroll.cs
@@ -1,21 +1,39 @@
-﻿using System.Collections;
+﻿/* CatWalk
+ * 
+ * by Zac
+ * 7 Mar, 2019
+ * 
+ * This script hadnles scrolling up and down in Inventory & Shop
+ */
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ArrowScroll : MonoBehaviour
 {
-
     private bool mouseOver = false;
-    [SerializeField] GameObject shopIcon;
-    [SerializeField] GameObject itemManager;
-    [SerializeField] bool isUp;
-    [SerializeField] bool inStore;
-    [SerializeField] Transform shopAnchor;
-    [SerializeField] Transform closetAnchor;
+
+    [Header("READ-ONLY (do not change!)")] //TODO: I need a better solution for this. LOL
+    [Tooltip("")] //ZAC: What are you using isUp for? i see it only as if(isUp) in this script, does it ever change to false or true?
+    [SerializeField] private bool isUp; //ZAC: Are these supposed to be read-only? why are you showing in inspector, for debugging? should we initialize these values?
+    [SerializeField] private bool inStore; //ZAC: Are these supposed to be read-only? why are you showing in inspector, for debugging? should we initialize these values?
+
+    [Header("Unity Setup Fields")]
+    [SerializeField] private GameObject shopIcon;
+    [SerializeField] private GameObject itemManager;
+    [SerializeField] private Transform shopAnchor; //ZAC: is shopAnchor's position sensitive to inspector or set in code? Where does shopAnchor have to be?
+    [SerializeField] private Transform closetAnchor;
 
 
     void Start()
     {
+        /* This sets Scale to (0,0,0) the images do not appear at the start
+         * //ZAC: would disable be beter? or do you need 0,0,0 because you're lerping?
+         * Modifies: localScale
+         * Returns: -
+         */
         transform.localScale = Vector3.zero;
     }
 
@@ -23,7 +41,10 @@ public class ArrowScroll : MonoBehaviour
     void Update()
     {
 
-        
+         /* Reads mouse-click inputs.
+         * Modifies: booleans in the ItemManager script (storeUp, storeDown, closetUp, closetDown)
+         * Returns: -
+         */
         if (mouseOver && Input.GetMouseButtonDown(0))
         {
             // store scroll
